2335bda2b6eaab13cfcc1f9e7e3a262c0f2949ba
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-07-11 18:22:23 -0400

    maquetacion de html y css, consumiendo la API

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
+const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=`;
 
-console.log("klk");
+const getTrendingMoviesPreview = async () => {
+  const res = await fetch(`${API_URL}${API_KEY}`);
+  const data = await res.json();
+
+  const movies = data.results;
+  console.log(data, movies);
+
+  movies.map((movie) => {
+    const trendingPreviewMoviesContainer = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
+
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
+    movieImg.setAttribute("src", `${BASE_IMG_URL}${movie.poster_path}`);
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer)
+  });
+};
+
+getTrendingMoviesPreview();
