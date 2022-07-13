5fae899db9d6319db7f4d0323c438084748e64ca
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-07-13 18:00:29 -0400

    integrando libreria de axios y modificando las vistas y las navegaciones

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
-const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=`;
+// const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=`;
+// const API_URL_CATEGORIES = `https://api.themoviedb.org/3/genre/movie/list?api_key=`;
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    api_key: API_KEY,
+  },
+});
 
 const getTrendingMoviesPreview = async () => {
-  const res = await fetch(`${API_URL}${API_KEY}`);
-  const data = await res.json();
-
+  const { data } = await api("trending/movie/day");
   const movies = data.results;
-  console.log(data, movies);
 
   movies.map((movie) => {
-    const trendingPreviewMoviesContainer = document.querySelector(
+    const trendingMoviesPreviewList = document.querySelector(
       "#trendingPreview .trendingPreview-movieList"
     );
 
@@ -21,8 +29,54 @@ const getTrendingMoviesPreview = async () => {
     movieImg.setAttribute("src", `${BASE_IMG_URL}${movie.poster_path}`);
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer)
+    trendingMoviesPreviewList.appendChild(movieContainer);
   });
 };
 
-getTrendingMoviesPreview();
+const getCategoriesPreview = async () => {
+  const { data } = await api("genre/movie/list");
+  const categories = data.genres;
+
+  categories.map((category) => {
+    const categoriesPreviewList = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
+
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.setAttribute("id", category.id);
+
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    categoriesPreviewList.appendChild(categoryContainer);
+  });
+};
+
+
+
+// function solution(n) {
+//   let array = [];
+//   let va = n.toString();
+//   let sum = 0;
+
+//   for (let item of va) {
+//     if (typeof item === "string") {
+//       let change = Number(item);
+//       console.log(va.length);
+//       array.push(change);
+//       if (sum !== 9) {
+//          (sum = sum + change);
+
+//         console.log(sum);
+//       }
+//       // console.log(array)
+//     }
+//   }
+// }
+
+// solution(45);
